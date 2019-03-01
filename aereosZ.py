4b3e46d025a03a4d7b4f2e517fc26213ef100c66
Changuitoss <ezealvarezrubio@gmail.com>
2019-03-01 15:41:51 -0300

    "Ignora REVERTIDOS, y toy intentando agregar mail"

--- aereosZ.py
+++ aereosZ.py
@@ -1,6 +1,6 @@
 #! python3
 
-import datetime, openpyxl
+import datetime, openpyxl, pyautogui, pyperclip
 
 aereosWb = openpyxl.load_workbook('O:\--- AEREOS INTEGRACION ---.xlsx')
 
@@ -8,20 +8,46 @@ sheets = aereosWb.sheetnames
 
 now = datetime.datetime.now()
 
+direccion = 'cdandolo@amichi.com.ar'
+asunto = 'Revertir *Z INTEGRACION'
+aereosRevertir = []
+
 
 for e in sheets:
     sheet = aereosWb[e]
     for i in range(sheet['B6'].row, sheet.max_row + 1, 2):
         valor = sheet['B' + str(i)].value
+        estado = sheet['O' + str(i)].value
+        if estado == 'REVERTIDO':
+            continue
         if valor != None:
             salida = datetime.datetime.strptime(sheet['B' + str(i)].value, '%d/%m/%Y')
             diasAntes30 = salida - datetime.timedelta(days=30)
         if diasAntes30 <= now:
-            print('Revertir el aéreo ' + sheet.title + ' ' + str(salida))
+            aereosRevertir.append('Revertir el aéreo ' + sheet.title + ' ' + str(salida))
         else:
             continue
-
+        
+aereosWb.close()
+        
 #TODO: Evitar los aereos que ya fueron revertidos
+
+def enviarMail():
+    mailProc = subprocess.Popen('C:\Program Files\Microsoft Office\OFFICE11\OUTLOOK.EXE')
+    pyautogui.click(36, 32)  #nuevo
+    pyperclip.copy(direccion)
+    pyperclip.paste(direccion)
+    pyautogui.click(133, 157) #asunto
+    pyperclip.copy(asunto)
+    pyperclip.paste(asunto)
+    pyautogui.click(66, 270)  #cuerpo
+    pyperclip.copy(aereosRevertir)
+    pyperclip.paste(aereosReverir)
+    pyautogui.click(31, 55)
+    
+    
+    
+
 
 #TODO: Enviar un mensaje de alguna forma a Cata
 
